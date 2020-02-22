a4a5aff4db7921e76e506ce566bfa2850ba47ffb
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-22 14:43:11 +0300

    Project Created

---
+++ YemekSiparisKata.NunitTest/YemekSiparisMotoruTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace YemekSiparisKata.NunitTest
+{
+    [TestFixture]
+    public class YemekSiparisMotoruTest
+    {
+        [SetUp]
+        public void Init()
+        { /* ... */ }
+
+        [TearDown]
+        public void Cleanup()
+        { /* ... */ }
+
+        [Test]
+        public void Add()
+        {
+            //given(arrange)
+
+            //when(act)
+
+            //then(assert)
+        }
+    }
+}
---
+++ YemekSiparisKata/YemekSiparisMotoru.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YemekSiparisKata
+{
+    public class YemekSiparisMotoru
+    {
+    }
+}
